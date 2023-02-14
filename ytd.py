eb8f438f531eddb2aa25c18f2fe37be20846bfcd
geode42 <99060684+geode42@users.noreply.github.com>
2023-02-13 23:22:05 -0500

    Add files via upload

--- ytd.py
+++ ytd.py
@@ -3,100 +3,86 @@ from pytube import YouTube
 import pytube
 import argparse
 import tempfile
-from os import system, makedirs
+from os import system
 from os.path import isfile, isdir, expanduser, abspath
-from time import time
 import platform
 import sys
-import yaml
+from rich import progress
+from rich.console import Console
+import shutil
 
+# Pyperclip is optional but recommended
+# It allows you to type in "ytd -c", which tells this script to get the URL from your clipboard
 using_pyperclip = False
 try:
 	import pyperclip
 	using_pyperclip = True
 except ModuleNotFoundError: pass
 
-default_config = """default_quality: 4K  # The max quality used when no quality is specified
-output_location: ~/Downloads  # Where videos are downloaded to
-ffmpeg_path: bundled  # If "bundled", the bundled ffmpeg will be used (won't work if you're not using the pre-compiled version). If left blank, the ffmpeg command will be used. If filled in with something else, the ffmpeg executable from this path will be used
+DEFAULT_QUALITY = '8K'
+# The max video resolution when not explicitly given through "-q" or "--quality"
+# It's 8K by default, because 144p feels too low and anything in between feels subjective
+# You probably want to lower this to 4K or 1080p or something
+# Speaking of, any way you can think of to specify the quality will *probably* will work.
+# As an example, 2160, 2160p, 4k, uhd, "ultra hd", "ultra high definition", and "UlTrA-h   DEfiNItiON" are all valid formats for "2160p"
 
-progress_bar:
-  length: 40
-  unit_color: [140, 140, 140]
-  completed_color: [255, 50, 50]
-  not_completed_color: [140, 140, 140]"""
+OUTPUT_LOCATION = './'
+# This is the directory where output files will be saved
+# Since this is a terminal program, the CWD felt most appropriate
+# You could change this to whatever you want though;
+# You might want to set this to "~/Downloads", for example
 
-config_folder = expanduser('~/.ytd')
-CONFIG_FILE = config_folder + '/config.yaml'
-if not isdir(config_folder):
-	makedirs(config_folder)
-if not isfile(CONFIG_FILE):
-	print('Creating config file...')
-	with open(CONFIG_FILE, 'w') as f:
-		f.write(default_config)
-	print(f'Config file created at {CONFIG_FILE}')
+FFMPEG_PATH = 'ffmpeg'
+# This script uses FFmpeg to combine the downloaded video and audio files (YT likes keeping them as two separate files)
+# I'd recommend installing FFmpeg if you don't have it already, it's pretty useful for things like compressing or re-encoding
+# Alternatively, you could place the ffmpeg executable ("ffmpeg" or "ffmpeg.exe") next to this script, then change this to "./ffmpeg"
 
-try:
-	with open(CONFIG_FILE) as f:
-		config = yaml.safe_load(f.read())
-		DEFAULT_QUALITY = config['default_quality']
-		OUTPUT_LOCATION = config['output_location']
-		FFMPEG_PATH = config['ffmpeg_path']
-		progressbar_config = config['progress_bar']
-		PROGRESS_BAR_LENGTH = progressbar_config['length']
-		PROGRESS_BAR_UNIT_COLOR = progressbar_config['unit_color']
-		PROGRESS_BAR_COMPLETED_COLOR = progressbar_config['completed_color']
-		PROGRESS_BAR_NOT_COMPLETED_COLOR = progressbar_config['not_completed_color']
-except FileNotFoundError:
-	print('Config file not found')
-	sys.exit(1)
+
+console = Console(highlight=False)
 
 # Get the args
 parser = argparse.ArgumentParser()
 parser.add_argument('url', nargs='?', help='The URL of the video; this can as short as it\'s 11-character long ID, or as long as the full URL displayed in your browser')
-parser.add_argument('-q', '--quality', help="Overrides the max resolution defined in the config")
-parser.add_argument('-f', '--filename', help='The filename of the downloaded video')
+parser.add_argument('-q', '--quality', help="Overrides the max resolution defined at the top of this python script (this is kinda awkward if you compiled this...)")
+parser.add_argument('-f', '--filename', help='The filename of the downloaded video (just the stem, the extension is added automatically)')
 parser.add_argument('-c', '--clipboard', help='Gets the url from your clipboard', action='store_true')
-parser.add_argument('-o', '--output_directory', help='Overrides the output directory defined in the config')
+parser.add_argument('-o', '--output_directory', help='Overrides the output directory defined at the top of this python script (again, kinda awkward if you compiled this...)')
 
 args = parser.parse_args()
 
-# This is Useful in general
-CLEARLINESTRING = '\033[2K\r' # "\033[2K" clears the entire line ("\r" only clears the line up to the cursor's current position), "\r" moves the cursor back to the start of the line
+def printerr(message: str):
+	console.print(message, style='bold red')
 
-# Get the first part of the command (that will be run at the end)
-command_start = FFMPEG_PATH if FFMPEG_PATH else 'ffmpeg'
-if FFMPEG_PATH == 'bundled':
-	command_start = sys._MEIPASS + '/ffmpeg'
-elif FFMPEG_PATH:
-	if not isfile(FFMPEG_PATH):
-		print('FFmpeg executable defined in config not found')
-		sys.exit(1)
+# Check if the if the FFmpeg path will work
+if shutil.which(FFMPEG_PATH) is None and not isfile(FFMPEG_PATH):
+	printerr('FFmpeg executable not found')
+	sys.exit(1)
 
 # Get output directory
 if args.output_directory:
 	OUTPUT_LOCATION = args.output_directory
 
-OUTPUT_LOCATION = expanduser(OUTPUT_LOCATION)  # Allow using ~
+OUTPUT_LOCATION = expanduser(OUTPUT_LOCATION)  # Make things like "~/Downloads" work
 if not isdir(OUTPUT_LOCATION):
-	print("The output location doesn't exist")
+	printerr("The output location doesn't exist")
+	sys.exit(1)
+
+# Make sure URL is given correctly
+if args.clipboard and not using_pyperclip:
+	printerr('Your Python Interpreter needs to have the pyperclip installed in order to use the -c | --clipboard flag')
+	sys.exit(1)
+if args.clipboard and args.url:
+	printerr("url and -c | --clipboard can't be used together")
+	sys.exit(1)
+if not args.clipboard and not args.url:
+	printerr('One of the following arguments must be used: url, -c | --clipboard')
 	sys.exit(1)
 
 # Get URL
-if args.clipboard:
-	if not using_pyperclip:
-		print('Your Python Interpreter needs to have the pyperclip installed in order to use the -c | --clipboard flag')
-		sys.exit(1)
-
-	if args.url:
-		print("url and -c | --clipboard can't be used together")
-		sys.exit(1)
-	url = pyperclip.paste()
-else:
-	if not args.url:
-		print('One of the following arguments must be used: url, -c | --clipboard')
-		sys.exit(1)
-	url = args.url
+if args.clipboard: url = pyperclip.paste()
+if args.url: url = args.url
+# pytube requires the url to be at least v=vid_id, I think the vid_id should be enough though
+if len(url) == 11: url = 'v=' + url
 
 # This function converts user-friendly resolutions into the boring resolutions that pytube understands
 def convert_res(res):
@@ -130,129 +116,112 @@ def convert_res(res):
 if args.quality is None: args.quality = DEFAULT_QUALITY
 args.quality = convert_res(args.quality)
 if args.quality is False:
-	print("The quality isn't valid")
+	printerr("The quality isn't valid")
 	sys.exit(1)
-
-
-
-def on_progress(chunk: bytes, file_handler: BinaryIO, bytes_remaining: int):
-	if current_downloading == 'video': bytes_downloaded = videototalbytes - bytes_remaining
-	else: bytes_downloaded = totalbytes - bytes_remaining
-
-	bar = ''
-	bar += '\033[2K\r'
-
-	progress_bar_completed_color = f'\033[38;2;{PROGRESS_BAR_COMPLETED_COLOR[0]};{PROGRESS_BAR_COMPLETED_COLOR[1]};{PROGRESS_BAR_COMPLETED_COLOR[2]}m'
-	progress_bar_not_completed_color = f'\033[38;2;{PROGRESS_BAR_NOT_COMPLETED_COLOR[0]};{PROGRESS_BAR_NOT_COMPLETED_COLOR[1]};{PROGRESS_BAR_NOT_COMPLETED_COLOR[2]}m'
-	unit_color = f'\033[38;2;{PROGRESS_BAR_UNIT_COLOR[0]};{PROGRESS_BAR_UNIT_COLOR[1]};{PROGRESS_BAR_UNIT_COLOR[2]}m'
-	
-	characters_completed = int(bytes_downloaded / totalbytes * PROGRESS_BAR_LENGTH)
-	percent_completed = int(bytes_downloaded / totalbytes * 100)
-	
-	time_left = round((time() - start_time) / bytes_downloaded * (totalbytes - bytes_downloaded))
-
-
-	bar += f'{percent_completed}{unit_color}%'.rjust(4 + len(unit_color)) + '  '
-
-	bar += '\033[1m'
-	if characters_completed == 0:
-		bar += progress_bar_not_completed_color + '―' * PROGRESS_BAR_LENGTH
-	elif characters_completed >= PROGRESS_BAR_LENGTH - 1:
-		bar += progress_bar_completed_color + '―' * characters_completed + ' ' * (PROGRESS_BAR_LENGTH - characters_completed)
-	else:  # Normal
-		bar += progress_bar_completed_color + '―' * characters_completed + ' ' + progress_bar_not_completed_color + '―' * (PROGRESS_BAR_LENGTH - characters_completed - 1)
-	
-	bar += f'  \033[0m{time_left} {unit_color}seconds'
-
-	print(bar + '\033[0m', end='')
-
-# pytube requires the url to be at least v=vid_id, I think the vid_id should be enough though
-if len(url) == 11: url = 'v=' + url
 
 # Init yt object
-print(CLEARLINESTRING + 'Getting video info...', end='')
-try:
-	yt = YouTube(url, on_progress_callback=on_progress)
-except pytube.exceptions.RegexMatchError:
-	print(CLEARLINESTRING + "The URL isn't valid")
-	sys.exit(1)
+with console.status('Getting video info...'):
+	try:
+		yt = YouTube(url)
+	except pytube.exceptions.RegexMatchError:
+		printerr("The URL isn't valid")
+		sys.exit(1)
 
-# Return friendlier error messages
-try:
-	yt.check_availability()
-except pytube.exceptions.MembersOnly:
-	print(CLEARLINESTRING + "This utility can't download member-only videos")
-	sys.exit(1)
-except pytube.exceptions.RecordingUnavailable:
-	print(CLEARLINESTRING + 'This live stream recording is unavailable')
-	sys.exit(1)
-except pytube.exceptions.VideoUnavailable:
-	print(CLEARLINESTRING + 'This video is unavailable')
-	sys.exit(1)
-except pytube.exceptions.VideoPrivate:
-	print(CLEARLINESTRING + 'This video has been made private')
-	sys.exit(1)
-except pytube.exceptions.LiveStreamError:
-	print(CLEARLINESTRING + "This stream can't be downloaded")
-	sys.exit(1)
+	# Return friendlier error messages
+	try:
+		yt.check_availability()
+	except pytube.exceptions.MembersOnly:
+		printerr("This utility can't download member-only videos")
+		sys.exit(1)
+	except pytube.exceptions.RecordingUnavailable:
+		printerr('This live stream recording is unavailable')
+		sys.exit(1)
+	except pytube.exceptions.VideoUnavailable:
+		printerr('This video is unavailable')
+		sys.exit(1)
+	except pytube.exceptions.VideoPrivate:
+		printerr('This video has been made private')
+		sys.exit(1)
+	except pytube.exceptions.LiveStreamError:
+		printerr("This stream can't be downloaded")
+		sys.exit(1)
 
-# Get final filename
-if args.filename is None: args.filename = yt.title
-if platform.system() == 'Windows':
-	invalid_characters = '<', '>', ':', '"', '/', '\\', '|', '?', '*'
-	for c in invalid_characters: args.filename = args.filename.replace(c, '')
-	if len(OUTPUT_LOCATION) > 0 and not (OUTPUT_LOCATION.endswith('/') or OUTPUT_LOCATION.endswith('\\')):
-		OUTPUT_LOCATION += '\\'
-else:
-	args.filename = args.filename.replace('/', '')
-	if len(OUTPUT_LOCATION) > 0 and not OUTPUT_LOCATION.endswith('/'):
-		OUTPUT_LOCATION += '/'
-args.filename = OUTPUT_LOCATION + args.filename
-# If the file already exists, add a number in parentheses at the end, keep incrementing it until a valid output path is found
-if isfile(args.filename + '.mkv'):
-	num = 1
-	while isfile(f'{args.filename} ({num}).mkv'):
-		num += 1
-	args.filename = f'{args.filename} ({num})'
+with console.status('Getting filename...'):
+	# Get final filename
+	if args.filename is None: args.filename = yt.title
+	if platform.system() == 'Windows':
+		invalid_characters = '<', '>', ':', '"', '/', '\\', '|', '?', '*'
+		for c in invalid_characters: args.filename = args.filename.replace(c, '')
+		if len(OUTPUT_LOCATION) > 0 and not (OUTPUT_LOCATION.endswith('/') or OUTPUT_LOCATION.endswith('\\')):
+			OUTPUT_LOCATION += '\\'
+	else:
+		args.filename = args.filename.replace('/', '')
+		if len(OUTPUT_LOCATION) > 0 and not OUTPUT_LOCATION.endswith('/'):
+			OUTPUT_LOCATION += '/'
+	args.filename = OUTPUT_LOCATION + args.filename
+	# If the file already exists, add a number in parentheses at the end, keep incrementing it until a valid output path is found
+	if isfile(args.filename + '.mkv'):
+		num = 1
+		while isfile(f'{args.filename} ({num}).mkv'):
+			num += 1
+		args.filename = f'{args.filename} ({num})'
 
+with console.status('Getting quality...'):
+	# Get max video res
+	max_res = yt.streams.filter(only_video=True).order_by('resolution').last().resolution
+	if int(args.quality[:-1]) > int(max_res[:-1]):
+		args.quality = max_res
 
-# Get max video res
-max_res = yt.streams.filter(only_video=True).order_by('resolution').last().resolution
-if int(args.quality[:-1]) > int(max_res[:-1]):
-	args.quality = max_res
+with console.status('Getting streams...'):
+	videostream = yt.streams.filter(only_video=True, is_dash=True, res=args.quality).order_by('bitrate').last()
+	audiostream = yt.streams.filter(only_audio=True, is_dash=True).order_by('bitrate').last()
 
-# Get final streams
-videostream = yt.streams.filter(only_video=True, is_dash=True, res=args.quality).order_by('bitrate').last()
-audiostream = yt.streams.filter(only_audio=True, is_dash=True).order_by('bitrate').last()
-# Get filesizes (for progress bar)
-videototalbytes = videostream.filesize
-audiototalbytes = audiostream.filesize
-totalbytes = videototalbytes + audiototalbytes
+with console.status('Getting temp dir...'):
+	tmpdir = tempfile.TemporaryDirectory()
 
-# Create temporary directory for storing video and audio files before they're combined into the final destination
-tmpdir = tempfile.TemporaryDirectory()
+stream_progress_object = progress.Progress(
+	progress.TextColumn('[progress.description]{task.description}'),
+	progress.BarColumn(),
+	progress.TaskProgressColumn(),
+	progress.TimeRemainingColumn(),
+	progress.TransferSpeedColumn(),
 
+	auto_refresh=False
+)
 
-current_downloading = 'video'
-try:
-	start_time = time()
+with stream_progress_object as progress:
+	download_video_task = progress.add_task(description='Downloading video...', total=videostream.filesize)
+	def on_progress_video(chunk: bytes, file_handler: BinaryIO, bytes_remaining: int):
+		progress.update(download_video_task, advance=len(chunk), refresh=True)
+		file_handler.write(chunk)
+		
+	videostream.on_progress = on_progress_video
+
+	progress.start_task(download_video_task)
 	videostream.download(tmpdir.name, 'video')
-	current_downloading = 'audio'
+
+	download_audio_task = progress.add_task(description='Downloading audio...', total=audiostream.filesize)
+	def on_progress_audio(chunk: bytes, file_handler: BinaryIO, bytes_remaining: int):
+		progress.update(download_audio_task, advance=len(chunk), refresh=True)
+		file_handler.write(chunk)
+	audiostream.on_progress = on_progress_audio
+
+	progress.start_task(download_audio_task)
 	audiostream.download(tmpdir.name, 'audio')
-	print('\033[0m\033[2K\rCombining files with FFmpeg...', end='')
+
+with console.status('Joining files with FFmpeg...'):
 	command = (
-		command_start,
-		'-loglevel', 'warning',
-		'-i', tmpdir.name + '/' + 'video',
-		'-i', tmpdir.name + '/' + 'audio',
-		'-c', 'copy', '"' + args.filename.replace('"', '\\"') + '.mkv' + '"'
+			FFMPEG_PATH,
+			'-loglevel', 'warning',
+			'-i', tmpdir.name + '/' + 'video',
+			'-i', tmpdir.name + '/' + 'audio',
+			'-c', 'copy', '"' + args.filename.replace('"', '\\"') + '.mkv' + '"'
 	)
 	command = ' '.join(command)
 	system(command)
-	if isfile(abspath(args.filename + ".mkv")):
-		print(f'\033[2K\rVideo downloaded at {abspath(args.filename + ".mkv")}')
-	else:
-		print(f'Video not downloaded, perhaps due to the FFmpeg command not succeeding')
-		sys.exit(1)
-except KeyboardInterrupt:
-	print('\033[2K\rKeyboard Interrupt, canceling download...')
+
+
+if isfile(abspath(args.filename + ".mkv")):
+	console.print(f'Video downloaded at "[bright_magenta]{abspath(args.filename + ".mkv")}[/bright_magenta]"')
+else:
+	printerr(f'Video not downloaded, perhaps due to the FFmpeg command not succeeding')
